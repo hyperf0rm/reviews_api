aee992b221291ea40de6cb8dacec0e7e6be2c23a
Дмитрий Николайчик <drk2701@gmail.com>
2024-03-02 14:09:06 +0300

    optimize api.serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
+from rest_framework.validators import UniqueTogetherValidator
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from reviews.models import Title, Genre, Category, Review
 import datetime as dt
@@ -13,51 +13,44 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'bio', 'role')
+        fields = (
+            'username', 'email',
+            'first_name', 'last_name',
+            'bio', 'role')
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'bio', 'role')
+        fields = ('username', 'email',
+                  'first_name', 'last_name',
+                  'bio', 'role')
         read_only_fields = ('role',)
 
 
 class SignupSerializer(serializers.ModelSerializer):
-        email = serializers.EmailField(
-            max_length=254, 
-            required=True,
-            validators=[UniqueValidator(
-                queryset=User.objects.all(),
-                message='User with such email already exists.')])
-        username = serializers.RegexField(
-            regex=r'^[\w.@+-]+\Z',
-            max_length=150,
-            required=True,
-            validators=[UniqueValidator(
-                queryset=User.objects.all(),
-                message='User with such username already exists.')])
 
+    class Meta:
+        model = User
+        fields = ('username', 'email')
 
-        class Meta:
-            model = User
-            fields = ('username', 'email')
+    def validate_username(self, value):
 
-        def validate_username(self, value):
-             
-            if value.lower() == 'me':
-                raise serializers.ValidationError('Cannot use "me" as username.')
-            
-            if User.objects.filter(username=value).exists():
-                raise serializers.ValidationError('User with such username already exists.')
-            return value
-        
-        def validate_email(self, value):
-             
-            if User.objects.filter(email=value).exists():
-                raise serializers.ValidationError('User with such email already exists.')
-            return value
+        if value.lower() == 'me':
+            raise serializers.ValidationError('Cannot use "me" as username.')
+
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError(
+                'User with such username already exists.')
+        return value
+
+    def validate_email(self, value):
+
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError(
+                'User with such email already exists.')
+        return value
 
 
 class TokenObtainSerializer(TokenObtainPairSerializer):
@@ -72,9 +65,13 @@ class TokenObtainSerializer(TokenObtainPairSerializer):
 
         user = get_object_or_404(User, username=data.get('username'))
 
-        if not User.objects.filter(username=data.get('username'), confirmation_code=data.get('confirmation_code')).exists():
-            raise serializers.ValidationError('Wrong credentials, please check and try again.')
-        
+        if not User.objects.filter(
+            username=data.get('username'),
+            confirmation_code=data.get('confirmation_code')
+        ).exists():
+            raise serializers.ValidationError(
+                'Wrong credentials, please check and try again.')
+
         refresh = self.get_token(user)
         data['token'] = str(refresh.access_token)
 
@@ -83,7 +80,6 @@ class TokenObtainSerializer(TokenObtainPairSerializer):
 
 class GenreSerializer(serializers.ModelSerializer):
 
-
     class Meta:
         model = Genre
         exclude = ('id',)
@@ -91,22 +87,26 @@ class GenreSerializer(serializers.ModelSerializer):
 
 class CategorySerializer(serializers.ModelSerializer):
 
-
     class Meta:
         model = Category
         exclude = ('id',)
 
 
 class TitleSerializer(serializers.ModelSerializer):
-    genre = serializers.SlugRelatedField(many=True, slug_field='slug', queryset=Genre.objects.all())
-    category = serializers.SlugRelatedField(slug_field='slug', queryset=Category.objects.all())
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(),
+        slug_field='slug',
+        many=True
+    )
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(),
+        slug_field='slug'
+    )
     rating = serializers.ReadOnlyField()
-
 
     class Meta:
         model = Title
         fields = '__all__'
-        read_only_fields = ('id',)
 
     def __init__(self, *args, **kwargs):
         super(TitleSerializer, self).__init__(*args, **kwargs)
@@ -115,15 +115,17 @@ class TitleSerializer(serializers.ModelSerializer):
             self.fields['category'] = CategorySerializer()
 
     def validate_year(self, value):
-        if value > dt.datetime.now().year:
-            raise serializers.ValidationError('Enter a valid year.')
+        if value > dt.date.today().year:
+            raise serializers.ValidationError(
+                'The year cannot be greater than the current one.')
         return value
-    
+
 
 class ReviewSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(read_only=True, 
-                                          slug_field='username',
-                                          default=serializers.CurrentUserDefault())
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault())
 
     class Meta:
         model = Review
